10b63b48cd2943732d8764f0b77ec8c32f324716
Nazar Plechko <stalker14072001@gmail.com>
2024-03-29 00:38:33 +0200

    fixed bag

--- js/red line animation.js
+++
@@ -1,9 +0,0 @@
-var redLine = new ScrollMagic.Controller({ horizontal: false })
-var tween = TweenMax.to('#target', 0.5, {
-	backgroundColor: 'green',
-	width: '+=400',
-})
-var scene = new ScrollMagic.Scene({ triggerElement: '#line', duration: 500 })
-	.setTween(tween)
-	.addIndicators() // add indicators (requires plugin)
-	.addTo(redLine)
