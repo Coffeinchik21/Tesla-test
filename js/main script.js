f6182520a19444f5a64c3c55c324292c03bfb44b
Bohdan Stetsiuk <97448163+quisssum@users.noreply.github.com>
2024-03-31 02:07:41 +0200

    header 1.0 video 1.0

--- js/main script.js
+++ js/main script.js
@@ -1,6 +1,26 @@
 let menuBtn = document.querySelector('.burger-btn');
 let menu = document.querySelector('.burger-menu');
 menuBtn.addEventListener('click', function(){
-	menu.classList.toggle('burger-menu__active');
-    menuBtn.classList.toggle('burger-btn__active');
+	menu.classList.toggle('burger-menu--active');
+    menuBtn.classList.toggle('burger-btn--active');
 })
+
+var controller = new ScrollMagic.Controller({globalSceneOptions: {duration: 0, offset: 5}});
+
+	new ScrollMagic.Scene()
+					.setClassToggle(".header", "header--active") 
+					.addIndicators() 
+					.addTo(controller);
+	new ScrollMagic.Scene()
+                    .setClassToggle(".menu__link-item", "menu__link-item--active")
+					.addIndicators() 
+					.addTo(controller);
+    new ScrollMagic.Scene()
+                    .setClassToggle(".contact-number__item", "contact-number__item--active")
+					.addIndicators() 
+					.addTo(controller);
+
+
+
+
+
